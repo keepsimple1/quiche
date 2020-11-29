dcae776b1f28abec8871d9bdb1920d2b1307b8e3
Han Xu <keepsimple@gmail.com>
2020-11-28 17:37:53 -0800

    test larger INITIAL_WINDOW_PACKETS

--- src/recovery/mod.rs
+++ src/recovery/mod.rs
@@ -58,7 +58,7 @@ const RTT_WINDOW: Duration = Duration::from_secs(300);
 const MAX_PTO_PROBES_COUNT: usize = 2;
 
 // Congestion Control
-const INITIAL_WINDOW_PACKETS: usize = 10;
+const INITIAL_WINDOW_PACKETS: usize = 20;
 
 const INITIAL_WINDOW: usize = INITIAL_WINDOW_PACKETS * MAX_DATAGRAM_SIZE;
 
